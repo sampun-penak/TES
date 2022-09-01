f9276c689c9e620cf455de067312bd7dedfa92bf
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-01 13:07:56 +0800

    New
    New

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -3,7 +3,8 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import moment from 'moment-timezone'
 import knights from 'knights-canvas'
-export async function all(m) {
+let handler = m => m
+handler.all = async function (m) {
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
 	let name = await this.getName(who)
 	let a = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BOV","BRL","BSD","BTN","BWP","BYR","BZD","CAD","CDF","CHE","CHF","CHW","CLF","CLP","CNY","COP","COU","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MUR","MVR","MWK","MXN","MXV","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","USN","USS","UYI","UYU","UZS","VEF","VND","VUV","WST","XAF","XAG","XAU","XBA","XBB","XBC","XBD","XCD","XDR","XFU","XOF","XPD","XPF","XPT","XTS","XXX","YER","ZAR","ZMW"]
@@ -291,6 +292,7 @@ export async function all(m) {
 		global.knimg = kn.getRandom()
 	}
 }
+export default handler 
 
 function ucapan() {
 	const time = moment.tz('Asia/Jakarta').format('HH')
--- plugins/random-fitur-13.js
+++ plugins/random-fitur-13.js
@@ -5,12 +5,7 @@ import fs from 'fs'
 let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
-let pp = await conn.profilePictureUrl(who).catch(_ => hoppai.getRandom())
-
-// Fake 🤥
-let frep = { contextInfo: { externalAdReply: {title: global.wm, body: global.author, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')}}}
-let fdoc = {quoted:{key : {participant : '0@s.whatsapp.net'},message: {documentMessage: {title: `${command}`}}}}
-
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let imgr = flaaa.getRandom()
 
 if (command == 'savefile') {
@@ -30,104 +25,19 @@ if (!text) throw `where is the path?\n\nexample:\n${usedPrefix + command} plugin
     await fs.unlinkSync(text)
     m.reply(`Delete ${path} to file!`)
     }
-if (command == 'tesfake') {
-if (args[0] == 'aud') {
-await conn.sendMessage(m.chat, { audio: { url: 'http://www.myinstants.com/media/sounds/run-vine-sound-effect.mp3' },
-'fileLength': fsizedoc,
-'contextInfo': {
-'externalAdReply': {
-'showAdAttribution': true,
-'mediaUrl': 'https://www.facebook.com',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': '👋 Nihhh, ' ,
-'body': 'Role ',
-'thumbnail': await( await fetch(pp)).buffer(),
-'sourceUrl': gcwangsaf}}}, {quoted: fgif})
-}
-if (args[0] == 'vid') {
-await conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4' },
-'fileLength': fsizedoc,
-'contextInfo': {
-'externalAdReply': {
-'showAdAttribution': true,
-'mediaUrl': 'https://www.facebook.com',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': '👋 Nihhh, ' ,
-'body': 'Role ',
-'thumbnail': await( await fetch(pp)).buffer(),
-'sourceUrl': gcwangsaf}}}, {quoted: fgif})
-}
-if (args[0] == 'doc') {
-await conn.sendMessage(m.chat, { document: { url: 'http://www.myinstants.com/media/sounds/run-vine-sound-effect.mp3' },
-'fileName': wm,
-'fileLength': fsizedoc,
-'contextInfo': {
-'externalAdReply': {
-'showAdAttribution': true,
-'mediaUrl': 'https://www.facebook.com',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': '👋 Nihhh, ' ,
-'body': 'Role ',
-'thumbnail': await( await fetch(pp)).buffer(),
-'sourceUrl': gcwangsaf}}}, {quoted: fgif})
-}
-    }
     
 if (command == 'cekfake') {
- let frnya = ['ftroli', 'fkontak', 'fvn', 'floc', 'ftoko', 'fdocs', 'fgclink', 'fgif', 'fvid']
- if (!args[0]) return m.reply(Array.from(frnya))
- if (args[0] == 'ftroli') {
+try {
  await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
                 ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: ftroli})
- }
- if (args[0] == 'fkontak') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fkontak})
- }
- if (args[0] == 'fvn') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fvn})
- }
- if (args[0] == 'floc') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: floc})
- }
- if (args[0] == 'ftoko') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: ftoko})
- }
- if (args[0] == 'fdocs') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fdocs})
- }
- if (args[0] == 'fgclink') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fgclink})
- }
- if (args[0] == 'fgif') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fgif})
- }
- if (args[0] == 'fvid') {
- await conn.sendButton(m.chat, `Fake ${args[0]}`, wm, null, [
-                ['Ok!', `${usedPrefix + command}`]
-            ], m, {quoted: fvid})
- }
+            ], m, {quoted: args[0]})
+            } catch {
+            throw 'Errrr....'
+            }
     }
     
   }
-handler.command = ['savefile', 'cekfake', 'tesfake', 'savefile', 'openfile', 'removefile']
+handler.command = ['savefile', 'cekfake', 'savefile', 'openfile', 'removefile']
 handler.tags = ['host']
 
 export default handler
