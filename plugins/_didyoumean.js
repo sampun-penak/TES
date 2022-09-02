11cd3da8fbc474d0d6b4a8a0ba7dd63f664ced94
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-02 14:51:43 +0800

    New
    New

--- plugins/_didyoumean.js
+++ plugins/_didyoumean.js
@@ -1,5 +1,6 @@
 import didyoumean from 'didyoumean'
-export async function before(m, { conn, match, usedPrefix }) {
+import similarity from 'similarity'
+let handler = async(m, { conn, match, usedPrefix }) {
 	if ((usedPrefix = (match[0] || '')[0])) {
 		let noPrefix = m.text.replace(usedPrefix, '')
 		let args = noPrefix.trim().split` `.slice(1)
@@ -7,11 +8,10 @@ export async function before(m, { conn, match, usedPrefix }) {
 		let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
 		if (help.includes(noPrefix)) return
 		let mean = didyoumean(noPrefix, help)
-		let similarity = await(await import('similarity'))
 		let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
     let caption = `👋 Hai *${name} @${who.split("@")[0]}*\nApakah yang kamu maksud: *${usedPrefix + mean}*\nSimilarity: *${similarity(noPrefix, help)}%*`
 		if (mean) conn.sendButton(m.chat, caption, wm, null, [['✅ Iya', `${usedPrefix + mean} ${text}`], ['❌ Bukan', usedPrefix + '?']], m, { mentions: conn.parseMention(caption) })
 	}
 }
-export const disabled = false
+export default handler
