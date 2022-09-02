0f361212c81702ce24c258672ad43f9903470ee2
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-02 20:07:57 +0800

    New
    New

--- plugins/_didyoumean.js
+++ plugins/_didyoumean.js
@@ -9,10 +9,10 @@ export async function before(m, { match, usedPrefix, command }) {
 		let mean = didyoumean(noPrefix, help)
 		if (mean.includes(noPrefix)) return
 		let sim = similarity(noPrefix, mean)
-		let som = sim * 100
+		let som = sim * 1000
 		let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
-    let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Apakah yang kamu maksud: *${usedPrefix + mean}*\nSimilarity: *${som.slice(0, 3)}%*`
+    let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Apakah yang kamu maksud: *${usedPrefix + mean}*\nSimilarity: *${som}%*`
 		if (mean) this.sendButton(m.chat, caption, wm, null, [['✅ Iya', `${usedPrefix + mean} ${text}`], ['❌ Bukan', usedPrefix + '?']], m, { mentions: this.parseMention(caption) })
 	}
 }
