ddbf9fbe595b854d3098143bb4979d2da6e409ee
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-02 15:41:28 +0800

    New
    New

--- plugins/_didyoumean.js
+++ plugins/_didyoumean.js
@@ -1,17 +1,18 @@
 import didyoumean from 'didyoumean'
 import similarity from 'similarity'
-let handler = async(m, { conn, match, usedPrefix }) {
+export async function before(m, { match, usedPrefix, command}) {
 	if ((usedPrefix = (match[0] || '')[0])) {
 		let noPrefix = m.text.replace(usedPrefix, '')
 		let args = noPrefix.trim().split` `.slice(1)
 		let text = args.join` `
 		let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
+		let sim = similarity(noPrefix, help)
 		if (help.includes(noPrefix)) return
 		let mean = didyoumean(noPrefix, help)
 		let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
-    let caption = `👋 Hai *${name} @${who.split("@")[0]}*\nApakah yang kamu maksud: *${usedPrefix + mean}*\nSimilarity: *${similarity(noPrefix, help)}%*`
-		if (mean) conn.sendButton(m.chat, caption, wm, null, [['✅ Iya', `${usedPrefix + mean} ${text}`], ['❌ Bukan', usedPrefix + '?']], m, { mentions: conn.parseMention(caption) })
+    let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Apakah yang kamu maksud: *${usedPrefix + mean}*\nSimilarity: *${sim}%*`
+		if (mean) this.sendButton(m.chat, caption, wm, null, [['✅ Iya', `${usedPrefix + mean} ${text}`], ['❌ Bukan', usedPrefix + '?']], m, { mentions: this.parseMention(caption) })
 	}
 }
-export default handler
+export const disabled = false
