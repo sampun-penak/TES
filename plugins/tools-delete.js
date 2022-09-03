09203f629b21bd4762acb491a196d3edce7f6ad7
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-03 08:32:36 +0800

    New
    New

--- plugins/tools-delete.js
+++ plugins/tools-delete.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, command }) => {
 if (!m.quoted) throw 'Reply pesan yang ingin dihapus'
 try {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+ } catch {
 let meng = m.message.extendedTextMessage.contextInfo.participant
 let hapus = m.message.extendedTextMessage.contextInfo.stanzaId
 return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: hapus, participant: meng }})
- } catch {
-    return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
 }
 }
 handler.help = ['del', 'delete']
