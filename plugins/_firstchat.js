1a9d5c8dc6280572f9dc0f307716c28af5c412ef
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-05 14:42:46 +0800

    New
    New

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -6,8 +6,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup || db.data.settings[th
     if (new Date - user.pc < cooldown) return // setiap 24 jam sekali
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
-    let caption = `👋 Hai *${name} @${who.split("@")[0]}*, ${user.banned ? 'kamu dibanned' : `Ada yang bisa *${this.user.name}* bantu?\nKetik *.menu* untuk melihat list fitur bot`}
-`.trim()
+    let caption = `👋 Hai ${name} @${who.split("@")[0]}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa *${this.user.name}* bantu?\nKetik *.menu* untuk melihat list fitur bot`}`.trim()
     this.sendButton(m.chat, caption, wm, null, [[user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '/owner' : '/menu']], m, { mentions: this.parseMention(caption) })
     user.pc = new Date * 1
 }
