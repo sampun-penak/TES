83a9c23b4a5dd56886e2abdfa83e64a03e4681bf
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-06 06:57:59 +0800

    New
    New

--- plugins/menfes-start.js
+++ plugins/menfes-start.js
@@ -2,13 +2,14 @@ import fetch from 'node-fetch'
 let handler = async(m, {
 	conn, text, usedPrefix, command, args
 }) => {
-	let mention = m.mentionedJid[0] || (args[0].replace(/[@.+-]/g, '').replace(' ', '') + '@s.whatsapp.net') || ''
-	if(!mention) throw 'Tag salah satu lah'
+	if (!args[0]) return conn.reply(m.chat, `Silahkan masukan pesannya\nContoh Penggunaan: ${usedPrefix + command} ${nomorown} pesan untuknya`, fakes, fakeyt)
+	let mention
+    if (m.isGroup) mention = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else mention = m.sender
+	if (!mention) throw 'Tag salah satu lah'
 	let txt = (args.length > 1 ? args.slice(1).join(' ') : '') || ''
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
-	if(!txt) return conn.reply(m.chat, `Silahkan masukan pesannya`, fakes, fakeyt)
-	if(txt > 700) return conn.reply(m.chat, 'Teks Kepanjangan!', fakes, fakeyt)
 	let pengirim = m.sender
 	let tujuan = `👋 Saya *${conn.user.name}*, Pesan Untuk Kamu
 👥 Dari : wa.me/${pengirim.split("@s.whatsapp.net")[0]}
@@ -20,10 +21,10 @@ ${htjava} ${txt}
 Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
 mantan?, tapi Tidak ingin tau siapa Pengirimnya?
 Kamu bisa menggunakan Bot ini
-Contoh Penggunaan: ${usedPrefix + command} pesan untuknya
+Contoh Penggunaan: ${usedPrefix + command} ${nomorown} pesan untuknya
 
-Contoh: ${usedPrefix + command} hai`
-	if(!m.quoted) {
+Contoh: ${usedPrefix + command} ${nomorown} hai`
+	if (!m.quoted) {
 		await conn.sendHydrated(mention, tujuan, cap, thumbnailUrl.getRandom(), 'https://wa.me/' + pengirim.split("@s.whatsapp.net")[0], 'KIRIM PESAN', null, null, [
 			[null, null]
 		], null)
