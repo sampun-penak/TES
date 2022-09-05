93adea4fafabc67f6b188ed8104af7be837cfc60
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-05 16:39:03 +0800

    Delete menfes-ans.js

--- plugins/menfes-ans.js
+++
@@ -1,6 +0,0 @@
-export async function before(m, { conn, isOwner }) {
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/(MENFES|MENCRET)!/i.test(m.quoted.text)) return !0
-    if (!isOwner) throw false
-    conn.reply(m.quoted.mentionedJid[0], '*Temanmu:*\n' htjava + m.text, m)
-    conn.reply(m.quoted.key.remoteJid, '*Pesan dari Temanmu:*\n' htjava + m.text, m)
-}
