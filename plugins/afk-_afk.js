01958dd39e593304c57f234384f24fd09081ff2c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-10 20:28:35 +0800

    New
    New

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,13 +2,11 @@ export function before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         let caption = `
-  *Kamu berhenti AFK* ${user.afkReason ? ' setelah ' + user.afkReason : ''}
+  *${conn.getName(m.sender)}* @${m.sender.split("@")[0]} *berhenti AFK* ${user.afkReason ? ' setelah ' + user.afkReason : ''}
   Selama ${(new Date - user.afk).toTimeString()}
   `.trim()
   let kataafk = ['mau turu', 'mau nyolong', 'Ke rumah ayang', 'jagain lilin', 'beli pop es', 'kawin lari', 'main kelereng', 'petak umpet', 'push renk', 'push up joni', 'olahraga', 'onani', 'beraq', 'open bo', 'di suruh emak', 'kerja']
-conn.sendButton(m.chat, caption, author, null, [
-        ['Afk Lagi', '.afk ' + kataafk.getRandom()]
-    ], m)
+    conn.sendButton(m.chat, caption, wm, null, [['AFK Lagi', '.afk ' + kataafk.getRandom()]], m, { mentions: conn.parseMention(caption) })
         user.afk = -1
         user.afkReason = ''
     }
@@ -22,13 +20,11 @@ conn.sendButton(m.chat, caption, author, null, [
             continue
         let reason = user.afkReason || ''
         let caption = `
-  *Jangan tag dia!*
+  *Jangan tag* *${conn.getName(jid)}* @${jid.split("@")[0]}!
   Dia sedang AFK *${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}*
   Selama ${(new Date - afkTime).toTimeString()}
   `.trim()
-conn.sendButton(m.chat, caption, author, null, [
-        ['Berhenti', '/tts id kok berhenti']
-    ], m)
+    conn.sendButton(m.chat, caption, wm, null, [['Berhenti', '/tts id kok berhenti']], m, { mentions: conn.parseMention(caption) })
     }
     return true
 }
