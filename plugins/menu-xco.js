dc4525cc4cff477741cbfcc564291c95fd6c101b
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-11 09:28:04 +0800

    New
    New

--- plugins/menu-xco.js
+++ plugins/menu-xco.js
@@ -19,7 +19,7 @@ let urut = text.split`|`
   let three = urut[3]
   
 let template = (args[0] || '').toLowerCase()
-if (!args[0]) {
+if (!args[0] || !one) {
 let caption = `*Contoh Penggunaan Single*
 ${usedPrefix + command} cecan
 
@@ -214,7 +214,7 @@ ${usedPrefix + command} pinterest |wibu
 • ${usedPrefix + command} waterwriting
 • ${usedPrefix + command} webp2mp4
 `
-await conn.sendButtonVid(m.chat, giflogo, caption, 'Nih.mp4', 'Back', '.menulist', fakes, adReply)
+await conn.sendButtonVid(m.chat, logo, caption, 'Nih.mp4', 'Back', '.menulist', fakes, adReply)
             }
             
 try {
