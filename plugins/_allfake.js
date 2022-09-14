be49ce56caa6699effe93d67b7a71af17a969711
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-14 21:26:13 +0800

    New
    New

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -8,17 +8,21 @@ export async function all(m) {
 	let sap = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann']
 	let a = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BOV','BRL','BSD','BTN','BWP','BYR','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','INR','IQD','IRR','ISK','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MXV','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SRD','SSP','STD','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYI','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XFU','XOF','XPD','XPF','XPT','XTS','XXX','YER','ZAR','ZMW']
     let b = a.getRandom()
-    let pp = await this.profilePictureUrl(m.sender, 'image')
+    let pp = await this.profilePictureUrl(m.sender).catch(_ => hwaifu.getRandom())
     
     // jpegThumbnail
     let situm = await this.resize(thumbnailUrl.getRandom(), 300, 150)
     let sipp = await this.resize(pp, 300, 300)
     // Fake Knights
-		let imagea = await new knights.Jo().setImage(pp).toBuild().toBuffer()
-		let imageb = await new knights.Patrick().setAvatar(pp).toAttachment().toBuffer()
-		let imagec = await new knights.Bonk().setAvatar1(pp).setAvatar2(pp).toBuild().toBuffer()
-		let imaged = await new knights.Burn().setAvatar(pp).toAttachment().toBuffer()
-		let kn = [imagea, imageb, imagec, imaged]
+		let imagea = await new knights.Jo().setImage(pp).toBuild();
+		let dataa = imagea.toBuffer();
+		let imageb = await new knights.Patrick().setAvatar(pp).toAttachment();
+		let datab = imageb.toBuffer();
+		let imagec = await new knights.Bonk().setAvatar1(pp).setAvatar2(pp).toBuild();
+		let datac = imagec.toBuffer();
+		let imaged = await new knights.Burn().setAvatar(pp).toAttachment();
+		let datad = imaged.toBuffer();
+		let kn = [dataa, datab, datac, datad]
 			// Mime Random
 		let pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 			// Fake Random
