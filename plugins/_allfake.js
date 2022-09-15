628d5d273965a95928612ad8bce228f6cf508ade
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-15 22:17:08 +0800

    New
    New

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,7 +7,7 @@ export async function all(m) {
 	let name = await this.getName(m.sender)
 	let sapa = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann'].getRandom()
 	let curr = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BOV','BRL','BSD','BTN','BWP','BYR','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','INR','IQD','IRR','ISK','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MXV','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SRD','SSP','STD','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYI','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XFU','XOF','XPD','XPF','XPT','XTS','XXX','YER','ZAR','ZMW'].getRandom()
-    let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.pinimg.com/736x/75/e4/30/75e430152909113cf266a6629dc7773f.jpg')
+    let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://www.seekpng.com/png/full/228-2289964_collection-of-free-hinata-drawing-naruto-girl-download.png')
     
     // jpegThumbnail
     let _situm = await this.resize(thumbnailUrl.getRandom(), 450, 300)
