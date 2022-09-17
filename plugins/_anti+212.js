096f324a9af982e8c7a4969f79a35435a5c79221
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-17 15:36:23 +0800

    New
    New

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -2,29 +2,21 @@
 export async function before(m, { isAdmin, isBotAdmin }) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply(htki + ' Banned Number ' + htka)
-conn.reply(htki + ' Kick User ' + htka)
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.reply(m.chat, htki + ' Banned Number ' + htka, m)
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply(htki + ' Banned Number ' + htka)
-conn.reply(htki + ' Kick User ' + htka)
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.reply(m.chat, htki + ' Banned Number ' + htka, m)
    } 
    
    if (m.sender.startsWith('92' || '92')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply(htki + ' Banned Number ' + htka)
-conn.reply(htki + ' Kick User ' + htka)
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.reply(m.chat, htki + ' Banned Number ' + htka, m)
    } 
    
    if (m.sender.startsWith('90' || '90')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply(htki + ' Banned Number ' + htka)
-conn.reply(htki + ' Kick User ' + htka)
-conn.groupParticipantsUpdate(m.chat, [user], "remove")
+conn.reply(m.chat, htki + ' Banned Number ' + htka, m)
    } 
     }
