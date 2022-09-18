abd1f85b7ab91fefffaaaf617271cf9bc1854542
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-18 12:56:58 +0800

    New
    New

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -3,16 +3,16 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import moment from 'moment-timezone'
 import knights from 'knights-canvas'
-export async function all(m) => {
-let who = m.sender ? m.sender : this.user.jid && this.user.jid ? this.user.jid : '0@s.whatsapp.net'
-	let name = await this.getName(who)
+export async function before(m, { conn }) {
+let who = m.sender ? m.sender : conn.user.jid && conn.user.jid ? conn.user.jid : '0@s.whatsapp.net'
+	let name = await conn.getName(who)
 	let sapa = ['Hai', 'Ohayo', 'Kyaa', 'Halo', 'Nyann'].getRandom()
 	let curr = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BOV','BRL','BSD','BTN','BWP','BYR','BZD','CAD','CDF','CHE','CHF','CHW','CLF','CLP','CNY','COP','COU','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','INR','IQD','IRR','ISK','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MXV','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SRD','SSP','STD','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','USN','USS','UYI','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XAU','XBA','XBB','XBC','XBD','XCD','XDR','XFU','XOF','XPD','XPF','XPT','XTS','XXX','YER','ZAR','ZMW'].getRandom()
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => hwaifu.getRandom())
+	let pp = await conn.profilePictureUrl(who, 'image').catch(_ => hwaifu.getRandom())
 	
     // jpegThumbnail
-    let _situm = await this.resize(thumbnailUrl.getRandom(), 300, 150)
-    let sipp = await this.resize(pp, 150, 150)
+    let _situm = await conn.resize(thumbnailUrl.getRandom(), 300, 150)
+    let sipp = await conn.resize(pp, 150, 150)
     
     // Fake Knights
 		let imagea = await new knights.Jo().setImage(pp).toBuild();
