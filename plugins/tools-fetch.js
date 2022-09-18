f2f328d504284b739f78d92ec25b2a748f5b4192
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-19 01:02:14 +0800

    New
    New

--- plugins/tools-fetch.js
+++ plugins/tools-fetch.js
@@ -4,23 +4,12 @@ import { format } from 'util'
 let handler = async (m, { text }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
-	if (!text) throw '*Masukkan Link*'
+if (!text) throw '*Masukkan Link*\n*Ex:* https://s.id'
+	if (text.includes('http')) {
 	let { href: url, origin } = new URL(text)
 	let res = await fetch(url, { headers: { 'referer': origin }})
 	if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) throw `Content-Length: ${res.headers.get('content-length')}`
-	if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, ucapan, author, m, null, { fileName: ucapan, pageCount: fpagedoc, fileLength: fsizedoc, seconds: fsizedoc, caption: author, contextInfo: {
-          externalAdReply :{
-    body: ucapan,
-    containsAutoReply: true,
-    mediaType: 2, 
-    mediaUrl: sfb,
-    showAdAttribution: true,
-    sourceUrl: sfb,
-    thumbnailUrl: logo,
-    renderLargerThumbnail: true,
-    title: '👋 Hai, ' + name,
-     }}
-  })
+	if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, ucapan, author, fakes, null, adReply)
 	let txt = await res.buffer()
 	try {
 		txt = format(JSON.parse(txt + ''))
@@ -28,6 +17,20 @@ let name = await conn.getName(who)
 		txt = txt + ''
 	} finally {
 		m.reply(txt.slice(0, 65536) + '')
+	}
+	} else {
+	let { href: url, origin } = new URL('https://' + text)
+	let res = await fetch(url, { headers: { 'referer': origin }})
+	if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) throw `Content-Length: ${res.headers.get('content-length')}`
+	if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, ucapan, author, fakes, null, adReply)
+	let txt = await res.buffer()
+	try {
+		txt = format(JSON.parse(txt + ''))
+	} catch (e) {
+		txt = txt + ''
+	} finally {
+		m.reply(txt.slice(0, 65536) + '')
+	}
 	}
 }
 handler.help = ['fetch']
