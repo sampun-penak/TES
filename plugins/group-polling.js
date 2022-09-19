cfe05547fbfa45c69ada9d635eb486bc14a331d9
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-09-19 13:22:09 +0800

    New
    New

--- plugins/group-polling.js
+++ plugins/group-polling.js
@@ -2,13 +2,13 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let a = []
 let b = text.split('|')
-for (let c = 0; c < b.length; c++) {
+for (let c = 1; c < b.length; c++) {
 a.push([b[c]])
 			}
-			return conn.sendPoll(m.chat, 'Polling Untuk:\n' + text, a, m)
+			return conn.sendPoll(m.chat, 'Polling Untuk:\n' + text.toUpperCase().split('|')[0], a, m)
 }
 
-handler.help = ['poll <teks>']
+handler.help = ['poll <text>|<text>']
 handler.tags = ['group'] 
 handler.command = /^poll(ing|s)?$/i
 
